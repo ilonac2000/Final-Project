7b44d0c1076193ed9a217f4feefa497199500caa
ilonac2000 <icherepakhina@stuy.edu>
2017-01-07 20:18:44 -0500

    the basics done

--- start.java
+++ start.java
@@ -1,10 +1,35 @@
-import javax.swing.JButton;
-import javax.swing.JComboBox;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.BoxLayout;
-import java.awt.Component;
+import javax.swing.*;
+import java.awt.*;
+public class sindow extends JFrame {
+ private Container pane;
+ private JLabel a;
+ private JButton map;
+ private JButton time;
+ private JButton date;
+ private JTextField t;
 
-public class start{
+ 
+ public start() {
+     this.setTitle("Starter");
+     this.setSize(600,400);
+     this.setLocation(100,100);
+     this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    
+     pane = this.getContentPane();
+     pane.setLayout(new FlowLayout());  
+     a = new JLabel("How do you want to see the data?");
+     map = new JButton("Map");
+     time = new JButton("Timeline");
+     date = new JButton("Choice a date");
+     pane.add(map);
+     pane.add(time);
+     pane.add(date);
+
+ }
+
+ 
+  public static void main(String[] args) {
+     start one = new start();
+     one.setVisible(true);
+  }
 }
