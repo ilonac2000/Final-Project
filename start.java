9f04ab1172890d741d4c18ed0577af27b78cbfdb
ilonac2000 <icherepakhina@stuy.edu>
2017-01-07 21:12:48 -0500

    more customization

--- start.java
+++ start.java
@@ -1,6 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
-public class Window extends JFrame {
+public class start extends JFrame {
  private Container pane;
  private JLabel a;
  private JButton map;
@@ -9,31 +9,32 @@ public class Window extends JFrame {
  private JTextField t;
 
  
- public Start() {
+  public start() {
      this.setTitle("Start");
      this.setSize(1200,600);
      this.setLocation(100, 10);
      this.setDefaultCloseOperation(EXIT_ON_CLOSE);  
      pane = this.getContentPane();
-     pane.setLayout(new GridLayout());
+     pane.setLayout(new FlowLayout());
      pane.setBackground(Color.black);
      a = new JLabel("How do you want to see the data?", JLabel.CENTER);
      a.setForeground(Color.white);
+     a.setFont(new Font("Arial", Font.BOLD, 72));
      map = new JButton("Map");
      time = new JButton("Timeline");
      date = new JButton("Choice a date");
      map.setFont(new Font("Arial", Font.PLAIN, 50));
      time.setFont(new Font("Arial", Font.PLAIN, 50));
      date.setFont(new Font("Arial", Font.PLAIN, 50));
+     pane.add(a);
      pane.add(map);
      pane.add(time);
      pane.add(date);
-
  }
 
  
   public static void main(String[] args) {
-     Start g = new Start();
-     g.setVisible(true);
+     start starter = new start();
+     starter.setVisible(true);
   }
 }
