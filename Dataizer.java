89085af577c8e26cde08581e5cdd0782d8c0a4b5
kvasilkin <kvasilkin@stuy.edu>
2017-01-09 18:53:14 -0500

    Add files via upload
    Very incomplete but has scanner

---
+++ Dataizer.java
@@ -0,0 +1,15 @@
+import java.util.*; 
+import java.io.*; 
+public class Dataizer{
+
+    public string[][][][][][]data;
+    public void organize (string fileName){
+	  try {
+	      Scanner sc = new Scanner(new File(fileName))).useDelimiter("</TR>");
+      		while(sc.hasNextLine()){
+            	       
+        }
+        catch (FileNotFoundException e){
+        	System.out.println("File Does Not Exist");
+        	System.exit(1);Scanner sc = new Scanner(new File(fileName));
+}
