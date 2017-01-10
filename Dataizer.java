ca6eaadd1191952fbd9ff671e89e3bee06366052
kvasilkin <kvasilkin@stuy.edu>
2017-01-10 04:51:31 -0500

    Add files via upload
    Error free still need to integrate the Arraylists into a multidimensional array

--- Dataizer.java
+++ Dataizer.java
@@ -2,23 +2,44 @@ import java.util.*;
 import java.io.*; 
 public class Dataizer{
 
-    //public string[][][][][][]data;
-   
-     public ArrayList<String> times  = new ArrayList<String>();
-     public ArrayList<String> places  = new ArrayList<String>();
-     public ArrayList<String> shape  = new ArrayList<String>();
+     
+
+    
     public static void organize (String fileName){
-        ArrayList<String> dates  = new ArrayList<String>();
-    try {  Scanner sc = new Scanner(new File(fileName)).useDelimiter("</TR>");
-      		while(sc.hasNextLine()){
-		    dates.add((sc.nextLine()).substring(102,111));      
-		}}
-    catch (FileNotFoundException e){
-        	System.out.println("File Does Not Exist");
-        	System.exit(1); } 
+     ArrayList<String> dates  = new ArrayList<String>();
+  ArrayList<String> times  = new ArrayList<String>();
+     ArrayList<String> places  = new ArrayList<String>();
+    ArrayList<String> shape  = new ArrayList<String>();
+  ArrayList<String> desc  = new ArrayList<String>();
+    ArrayList<String> posted  = new ArrayList<String>();
+
+
+
+	 
+    try{  Scanner sc = new Scanner(new File(fileName)).useDelimiter("\\s*delimit\\s*");
+	try{  	while(sc.hasNextLine()){
+		    dates.add((sc.nextLine()));
+		    places.add((sc.nextLine()));
+		    shape.add((sc.nextLine()));
+		    times.add((sc.nextLine()));
+		    desc.add((sc.nextLine()));
+		    posted.add((sc.nextLine()));
+	    }}
+	    catch(NoSuchElementException e){
+	    }
+
     }
+
+ catch (FileNotFoundException e){
+        	System.out.println("File Does Not Exist");
+        	System.exit(1);
+ }
+    }
+    
   public static void main(String[]args){
-      organize("UFOData.txt");}
+      organize("UFOData.txt");
+      // System.out.println(Arrays.toString(places));
+       }
 
 
     
