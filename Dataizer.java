5bd5fff3da36e7bbd1f1618480b012a2b639fd3d
kvasilkin <kvasilkin@stuy.edu>
2017-01-10 03:58:43 -0500

    Add files via upload
    Compile Error solved, new runtime error

--- Dataizer.java
+++ Dataizer.java
@@ -3,19 +3,22 @@ import java.io.*;
 public class Dataizer{
 
     //public string[][][][][][]data;
-    public ArrayList<String> dates  = new ArrayList<String>();
+   
      public ArrayList<String> times  = new ArrayList<String>();
      public ArrayList<String> places  = new ArrayList<String>();
      public ArrayList<String> shape  = new ArrayList<String>();
-    public void organize (String fileName){
-     Scanner sc = new Scanner(new File(fileName)).useDelimiter("</TR>");
+    public static void organize (String fileName){
+        ArrayList<String> dates  = new ArrayList<String>();
+    try {  Scanner sc = new Scanner(new File(fileName)).useDelimiter("</TR>");
       		while(sc.hasNextLine()){
-		    dates.add((sc.nextLine()).substring(102,11));      
-		}
+		    dates.add((sc.nextLine()).substring(102,111));      
+		}}
+    catch (FileNotFoundException e){
+        	System.out.println("File Does Not Exist");
+        	System.exit(1); } 
     }
-
   public static void main(String[]args){
-      organize(UfoData.txt);}
+      organize("UFOData.txt");}
 
 
     
