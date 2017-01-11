d2a7c157349dfda5d60d65172770fdeefc0d15e1
ilonac2000 <icherepakhina@stuy.edu>
2017-01-10 21:06:10 -0500

    added popup

--- Window3.java
+++ Window3.java
@@ -2,10 +2,13 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class Window3 extends JFrame {
+
+public class Window3 extends JFrame implements ActionListener{
     private Container pane;
     private JLabel a;
     private JButton year;
+    private JComboBox<String> cb; 
+    private JComboBox<String> cb2;
 
    public Window3() {
      this.setTitle("Calendar");
@@ -28,7 +31,7 @@ public class Window3 extends JFrame {
 
     String[] choices = { "January","February", "March","April","May","June","July","August","September","October","November","December"};
 
-    final JComboBox<String> cb = new JComboBox<String>(choices);
+    cb = new JComboBox<String>(choices);
 
     pane.add(cb);
     JLabel lbl2 = new JLabel("Day");
@@ -40,16 +43,62 @@ public class Window3 extends JFrame {
     for (int i = 0; i < 31; i++){
       choices2[i] = Integer.toString(i + 1);
     }
-    final JComboBox<String> cb2 = new JComboBox<String>(choices2);
+    cb2 = new JComboBox<String>(choices2);
 
     pane.add(cb2);
     year = new JButton("2016");
     pane.add(year);
+    year.addActionListener(this);
+    year.setActionCommand("result");
 }
+public static void PopUp(){
+        final JFrame parent = new JFrame();
+        JButton button = new JButton();
+        JOptionPane.showMessageDialog(null, "This Month Does Not Have 31 Days");
+        
+     }
+
+  public void actionPerformed(ActionEvent e){
+   String event = e.getActionCommand();
+   if(event.equals("result")){
+     String chosen = (String)cb.getSelectedItem();
+     String chosenDay = (String)cb2.getSelectedItem();
+     //System.out.print(chosen);
+     //System.out.println(chosenDay);
+    if (chosenDay.equals("31") && ((chosen.equals("February")) || 
+        chosen.equals("April") || 
+        chosen.equals("June") || 
+        chosen.equals("September")|| 
+        chosen.equals("November")))
+        {
+       // if (chosenDay == "31"){
+            System.out.println("NO");
+            //PopUp();
+     }
+     }
+ 
+}
+
+/*January - 31 days
+February - 28 days in a common year and 29 days in Leap Years
+March - 31 days
+April - 30 days
+May - 31 days
+June - 30 days
+July - 31 days
+August - 31 days
+September - 30 days
+October - 31 days
+November - 30 days
+December - 31 days*/
+
   public static void main(String[] args) {
      Window3 starter = new Window3();
-     starter.setVisible(true);
+     starter.setVisible(true);   
 }
     }
+
+
+
 
 
