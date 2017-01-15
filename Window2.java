0f4c0899f5e8e9e8365e4279e7b7c05c979e9e38
ilonac2000 <icherepakhina@stuy.edu>
2017-01-15 16:05:33 -0500

    map background, trying to fix label postioon

--- Window2.java
+++ Window2.java
@@ -1,22 +1,68 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+
+
 public class Window2 extends JFrame{
  private Container pane;
  private JLabel bkgrd;
+ private JLabel a;
 
- public Window2() {
-     this.setTitle("Places");
+ 
+public Window2() {
+     this.setTitle("Start");
+     this.setSize(1220,765);
+     this.setLocation(100, 10);
+     this.setDefaultCloseOperation(EXIT_ON_CLOSE);  
+     pane = this.getContentPane();
+     pane.setLayout(null);
+     a = new JLabel("32");
+     a.setFont(new Font("Arial", Font.BOLD, 40));
+     a.setLocation(10,10);
+     a.setBounds(100,100,200,30);
+     pane.add(a);
+     a.setOpaque(true);
+     try {
+    final Image backgroundImage = javax.imageio.ImageIO.read(new File("map.jpeg"));
+    setContentPane(new JPanel(new BorderLayout()) {
+        @Override public void paintComponent(Graphics g) {
+            g.drawImage(backgroundImage, 0, 0, null);
+        }
+    });
+} catch (IOException e) {
+    throw new RuntimeException(e);
+}
+ }
+
+/*public Window2(){
+	 
+
+
+}*/
+ 	/*JFrame f = new JFrame();
+    f.getContentPane().add(new Background("map.jpeg"));
+}*/
+     /*this.setTitle("Places");
      this.setSize(1200,765);
      this.setLocation(100, 10);
      this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 	 setLayout(new BorderLayout());
      bkgrd = new JLabel(new ImageIcon("map.jpeg"));
      add(bkgrd);
- }
+ } */
  public static void main(String[] args) {
      Window2 starter = new Window2();
      starter.setVisible(true);
 }
 }
+
+
 
