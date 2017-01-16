40b272c00f8f69db4dd1140ee796df6f1c153126
kvasilkin <kvasilkin@stuy.edu>
2017-01-15 19:31:47 -0500

    printing arraylist issues with arraylists

--- Dataizer.java
+++ Dataizer.java
@@ -2,12 +2,12 @@ import java.util.*;
 import java.io.*; 
 public class Dataizer{
      private static ArrayList<String> dates  = new ArrayList<String>();
-  private static ArrayList<String> times  = new ArrayList<String>();
-private static     ArrayList<String> city  = new ArrayList<String>();
+  private static ArrayList<String> city  = new ArrayList<String>();
+private static     ArrayList<String> state  = new ArrayList<String>();
  private static   ArrayList<String> shape  = new ArrayList<String>();
-private static  ArrayList<String> desc  = new ArrayList<String>();
-    private static ArrayList<String> posted  = new ArrayList<String>();
-       private static ArrayList<String> state  = new ArrayList<String>();
+private static  ArrayList<String> times  = new ArrayList<String>();
+    private static ArrayList<String> desc  = new ArrayList<String>();
+       private static ArrayList<String> posted  = new ArrayList<String>();
 
      
 
@@ -28,7 +28,7 @@ private static  ArrayList<String> desc  = new ArrayList<String>();
 		    desc.add((sc.next()));
 		    posted.add((sc.next()));
 	    }}
-	    catch(NoSuchElementException e){
+ 	    catch(NoSuchElementException e){
 	    }
 
     }
@@ -44,7 +44,15 @@ private static  ArrayList<String> desc  = new ArrayList<String>();
     
   public static void main(String[]args){
       organize("UFOData.txt");
+      //      System.out.println( Arrays.toString(dates.toArray()));
 
+      //  System.out.println( Arrays.toString(city.toArray()));
+      System.out.println( Arrays.toString(state.toArray()));
+      //	  System.out.println( Arrays.toString(shape.toArray()));
+            //System.out.println( Arrays.toString(times.toArray()));
+      //   System.out.println( Arrays.toString(desc.toArray()));
+      
+      //	    System.out.println( Arrays.toString(posted.toArray()));
        }
 
 
