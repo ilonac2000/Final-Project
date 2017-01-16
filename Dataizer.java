e4f409be44e534e2e0aedf06cef502f75f8d69b3
kvasilkin <kvasilkin@stuy.edu>
2017-01-15 23:35:00 -0500

    First step of state mapping done

--- Dataizer.java
+++ Dataizer.java
@@ -8,7 +8,7 @@ private static     ArrayList<String> state  = new ArrayList<String>();
 private static  ArrayList<String> times  = new ArrayList<String>();
     private static ArrayList<String> desc  = new ArrayList<String>();
        private static ArrayList<String> posted  = new ArrayList<String>();
-
+    private static int AL;
      
 
     
@@ -37,22 +37,32 @@ private static  ArrayList<String> times  = new ArrayList<String>();
         	System.out.println("File Does Not Exist");
         	System.exit(1);
  }
-    //   String[][][][] data = new String[dates.size()][dates.size()[dates.size()[dates.size();
+  
 	  
 									     
     }
-    
+      public static void countState (){
+int  AL = Collections.frequency(state, "AL");
+ 
+
+
+
+    }
+
   public static void main(String[]args){
       organize("UFOData2.txt");
-      //System.out.println( Arrays.toString(dates.toArray())); //desc
+      System.out.println(AL);
+      // int  occ = Collections.frequency(state, "NY");
+     //   System.out.println(occ);
+      //  System.out.println( Arrays.toString(dates.toArray())); 
 
-      //System.out.println( Arrays.toString(city.toArray())); //posted
-      //System.out.println( Arrays.toString(state.toArray())); //dates
-      //System.out.println( Arrays.toString(shape.toArray())); //city
-      // System.out.println( Arrays.toString(times.toArray())); //state
-       System.out.println( Arrays.toString(desc.toArray()));// shape
+      //System.out.println( Arrays.toString(city.toArray())); 
+      //System.out.println( Arrays.toString(state.toArray())); 
+      //System.out.println( Arrays.toString(shape.toArray())); 
+      // System.out.println( Arrays.toString(times.toArray())); 
+      // System.out.println( Arrays.toString(desc.toArray()));
       
-      //    System.out.println( Arrays.toString(posted.toArray())); //times
+      //    System.out.println( Arrays.toString(posted.toArray())); 
        }
 
 
