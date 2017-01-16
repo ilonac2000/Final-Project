f32e650f0e1ce8c6cab238d55dbfc9c27ccbafaa
ilonac2000 <icherepakhina@stuy.edu>
2017-01-15 20:15:27 -0500

    up to montana

--- Window2.java
+++ Window2.java
@@ -21,8 +21,51 @@ public class Window2 extends JFrame{
  private JLabel e;
  private JLabel f;
  private JLabel g;
+ private JLabel h;
+ private JLabel i;
+ private JLabel j;
+ private JLabel k;
+ private JLabel l;
+ private JLabel m;
+ private JLabel n;
+ private JLabel o;
+ private JLabel p;
+ private JLabel q;
+ private JLabel r;
+ private JLabel s;
+ private JLabel t;
+ private JLabel u;
+ private JLabel v;
+ private JLabel w;
+ private JLabel x;
+ private JLabel y;
+ private JLabel z;
+ private JLabel a1;
+ private JLabel b1;
+ private JLabel c1;
+ private JLabel d1;
+ private JLabel e1;
+ private JLabel f1;
+ private JLabel g1;
+ private JLabel h1;
+ private JLabel i1;
+ private JLabel j1;
+ private JLabel k1;
+ private JLabel l1;
+ private JLabel m1;
+ private JLabel n1;
+ private JLabel o1;
+ private JLabel p1;
+ private JLabel q1;
+ private JLabel r1;
+ private JLabel s1;
+ private JLabel t1;
+ private JLabel u1;
+ private JLabel v1;
+ private JLabel w1;
+ private JLabel x1;
+ private JLabel y1;
 
- 
 public Window2() {
      this.setTitle("Start");
      this.setSize(1220,765);
@@ -48,6 +91,22 @@ public Window2() {
             c.setBounds(100,100,100,100);
      		this.add(c);
      		c.setLocation(150,10);
+     		d = new JLabel("Idaho");
+            d.setBounds(100,100,100,100);
+     		this.add(d);
+     		d.setLocation(250,100);
+     		e = new JLabel("Nevada");
+            e.setBounds(100,100,100,100);
+     		this.add(e);
+     		e.setLocation(200,200);
+     		f = new JLabel("Montana");
+            f.setBounds(100,100,100,100);
+     		this.add(f);
+     		f.setLocation(400,50);
+     		//g = new JLabel("Montana");
+            //g.setBounds(100,100,100,100);
+     		//this.add(g);
+     		//g.setLocation(150,10);
         }
 
     });
@@ -81,9 +140,6 @@ public Window2() {
      starter.setVisible(true);
 }
 }
-
-
-
 
 
 
