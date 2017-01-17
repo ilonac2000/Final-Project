03a5b697e775fc434275b9604fb109c4941ea6b7
ilonac2000 <icherepakhina@stuy.edu>
2017-01-16 21:13:58 -0500

    works

--- Window1.java
+++ Window1.java
@@ -31,46 +31,44 @@ public class Window1 extends JFrame {
      Image cigar = javax.imageio.ImageIO.read(new File("cigar.jpg"));
      Image unknown = javax.imageio.ImageIO.read(new File("un.jpg"));
     setContentPane(new JPanel(new BorderLayout()) {
-        @Override
+       // @Override
          public void paintComponent(Graphics g){
             g.drawImage(backgroundImage, 0, 0, null);
             g.drawImage(triangle, 100, 200, null);
-            JLabel tri = new JLabel("<html> Triangle Shape <br>" + "</html>", JLabel.CENTER);
+            JLabel tri = new JLabel("<html> Triangle Shape <br>" + Dataizer.TRIANGLE + "</html>", JLabel.CENTER);
             tri.setBounds(100,300,200,140);
             tri.setForeground(Color.white);
      		tri.setFont(new Font("Arial", Font.BOLD, 20));
             this.add(tri);
             tri.setLocation(100,300); 
             g.drawImage(fire, 320, 200, null);
-            JLabel f = new JLabel("<html> Fireball Shape <br>" + "</html>", JLabel.CENTER);
-            f.setBounds(100,300,200,140);
+            JLabel f = new JLabel("<html> Fireball Shape <br>" + Dataizer.LIGHT + "</html>", JLabel.CENTER);
+            f.setBounds(330,300,200,140);
             f.setForeground(Color.white);
      		f.setFont(new Font("Arial", Font.BOLD, 20));
             this.add(f);
-            f.setLocation(330,300); 
+        //    f.setLocation(330,300); 
             g.drawImage(sphere, 570, 200, null);
-            JLabel s = new JLabel("<html> Sphere Shape <br>" + "</html>", JLabel.CENTER);
-            s.setBounds(100,300,200,140);
+            JLabel s = new JLabel("<html> Sphere Shape <br>" + Dataizer.SPHERE + "</html>", JLabel.CENTER);
+            s.setBounds(540,300,200,140);
             s.setForeground(Color.white);
      		s.setFont(new Font("Arial", Font.BOLD, 20));
             this.add(s);
-            s.setLocation(540,300); 
+        //    s.setLocation(540,300); 
             g.drawImage(cigar, 750, 200, null);
-            JLabel c = new JLabel("<html> Cigar Shape <br>" + "</html>", JLabel.CENTER);
-            c.setBounds(100,300,200,140);
+            JLabel c = new JLabel("<html> Cigar Shape <br>" + Dataizer.CIGAR + "</html>", JLabel.CENTER);
+            c.setBounds(750,300,200,140);
             c.setForeground(Color.white);
      		c.setFont(new Font("Arial", Font.BOLD, 20));
             this.add(c);
-            c.setLocation(750,300); 
+         //   c.setLocation(750,300); 
             g.drawImage(unknown, 985, 200, null);
-            JLabel u = new JLabel("<html> Unknown Shape <br>" + "</html>", JLabel.CENTER);
+            JLabel u = new JLabel("<html> Unknown Shape <br>" + Dataizer.UNKNOWN + "</html>", JLabel.CENTER);
             u.setBounds(960,300,200,140);
             u.setForeground(Color.white);
      		u.setFont(new Font("Arial", Font.BOLD, 20));
             this.add(u);
-            u.setLocation(965,300); 
-
-
+         //   u.setLocation(965,300); 
         }
     }
 
@@ -85,7 +83,9 @@ public class Window1 extends JFrame {
 
 }
  public static void main(String[] args) {
-
+     Dataizer.organize("UFOData2.txt");
+     Dataizer.dateFixer();
+     Dataizer.shapeCount();
      Window1 starter = new Window1();
      starter.setVisible(true);
 }
